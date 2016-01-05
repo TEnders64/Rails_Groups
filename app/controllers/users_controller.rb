3cf2b5bb5f6ccb3369681e0133690133ca5534c0
Todd Enders <tenders64@gmail.com>
2016-01-05 13:04:55 -0800

    updated create method to send json data back

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
   def new
+
   end
 
   def create
+  	user = User.new( user_params )
+  	user.password_confirmation = params[:user][:password_confirm]
+  	if user.valid?
+  		user.save
+  		flash[:message] = "User registered successfully, Please log in"
+  	else
+  		flash[:errors] = user.errors.full_messages
+  	end
+  	# redirect_to "/main"
+  	render json: {:message => "Hello World", :errors => "Errors go here"}
   end
+
+  private
+  	def user_params
+  		params.require(:user).permit(:first_name, :last_name, :email, :password)
+  	end
 end
