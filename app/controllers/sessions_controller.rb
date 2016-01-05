ecd94d118dcec99be71161fb63966cf8625f59c3
meadch <meadch@gmail.com>
2016-01-05 14:51:31 -0800

    completes login/reg

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,15 @@
 class SessionsController < ApplicationController
   def login
     puts 'login'
+
   	user = User.find_by_email(params[:user][:email])
   	if user && user.authenticate(params[:user][:password])
-  		session[:user_id] = user.id 
-  		render json: {:errors => false, :url => "/groups"}
+  		session[:user_id] = user.id
+  		redirect_to '/groups'
   	else
-      render json: {:errors => true, :message => ["User email/password do not match"]}
-  	end
+      flash[:errors] = "User email and/or password don't match"
+      redirect_to '/main'
+    end
   end
 
   def logout
