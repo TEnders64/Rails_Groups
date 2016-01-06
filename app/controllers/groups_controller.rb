0879938ac6ffdc90ecaea146979cc0fdb7e86b44
meadch <meadch@gmail.com>
2016-01-06 08:04:26 -0800

    minor fixes

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,9 +4,6 @@ class GroupsController < ApplicationController
   	@groups = Group.all
   	@user = User.select("first_name, last_name").find_by_id(session[:user_id])
   end
-  
-  def new
-  end
 
   def create
   	group = Group.new( groups_params )
@@ -18,8 +15,12 @@ class GroupsController < ApplicationController
   		# @groups = Group.all()
   		group = Group.last
   		count = group.members.count
+
+      # Create new authenticity token... code needs updating
   		authToken = form_authenticity_token
   		puts authToken
+      # /////////////////////////////
+
   		render json: {errors: false, group: group, count: count, token: authToken}
   	else
   		render json: {errors: true, messages: group.errors.full_messages}
